d1cc8b24a6ffef95bedd627c5363de20fc2b0ad3
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-17 13:07:16 +0100

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Soundwave - Gestionale Studi di Registrazione",
+  description: "Sistema gestionale cloud per studi di registrazione",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="it">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
